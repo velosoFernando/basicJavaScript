56d9ba83c96d299c168247c48d575ef1bd69465e
velosoFernando <fernandovelosomkt@gmail.com>
2025-11-14 02:20:07 -0300

    FEITO

---
+++ aulas/exeOpenAI/lógica js/exe003/exe003.js
@@ -0,0 +1,13 @@
+function multiplicar() {
+    var tn1 = document.querySelector('input#txtn1')
+    var tn2 = document.querySelector('input#txtn2')
+
+    var res = document.getElementById('res')
+
+    var n1 = Number(tn1.value)
+    var n2 = Number(tn2.value)
+
+    var multi = n1 * n2
+
+    res.innerHTML = `A multiplicação entre ${n1} e ${n2} gera o valor <strong>${multi}</strong>!`
+}
